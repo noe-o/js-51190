7285df4e26ca0a27e556d06a0dd5c2393ca03205
noe-o <107960650+noe-o@users.noreply.github.com>
2023-04-10 16:31:19 -0300

    armando el carrito

--- main.js
+++ main.js
@@ -7,39 +7,48 @@ compraAceite = () => {
   switch (prod) {
     case '1':
       suma = 800;
+      prod = prod + 'AOVE 250ml';
       break;
     case '2':
       suma = 1500;
+      prod = prod + 'AOVE 500ml';
       break;
     case '3':
       suma = 2700;
+      prod = prod + 'AOVE 1lt';
       break;
     case '4':
       suma = 4800;
+      prod = prod + 'AOVE 2lt';
       break;
     case '5':
       suma = 9400;
+      prod = prod + 'AOVE 5lt';
       break;
     default:
       console.log('el dato ingresado es incorrecto');
   }
 };
 compraCosmetica = () => {
-  let prod = prompt(
-    'Elige: 1.Jabón de aceite de olive 2.Jabón x 3 3.Jabonera rústica 4.Combo: 4 jabones + jabonera'
+  let elije = prompt(
+    'Elige: 1.Jabón de aceite de oliva 2.Jabón x3 3.Jabonera rústica 4.Combo: 4 jabones + jabonera'
   );
-  switch (prod) {
+  switch (elije) {
     case '1':
       suma = 700;
+      prod = prod + ' Jabón de aceite de oliva';
       break;
     case '2':
       suma = 1900;
+      prod = prod + ' Jabón x3';
       break;
     case '3':
       suma = 2000;
+      prod = prod + ' Jabonera rústica';
       break;
     case '4':
       suma = 4100;
+      prod = prod + ' Combo: 4 jabones + jabonera';
       break;
     default:
       console.log('el dato ingresado es incorrecto');
@@ -50,11 +59,11 @@ compraAceituna = () => {
   switch (elije) {
     case '1':
       suma = 1900;
-      prod = 'Aceituna negra';
+      prod = prod + ' Aceituna negra';
       break;
     case '2':
       suma = 1900;
-      prod = 'Aceituna verde';
+      prod = ' Aceituna verde';
       break;
     default:
       console.log('el dato ingresado es incorrecto');
@@ -78,10 +87,6 @@ compra = () => {
       console.log('el dato ingresado es incorrecto');
   }
   console.log(suma);
-  carrito = { prod };
-  const productos = [];
-  productos.push(carrito);
-  console.log(productos);
 };
 
 let valor = true;
@@ -101,8 +106,13 @@ while (valor) {
   } else alert('Responder: sí/no');
 }
 /*
-ticket = () => {
+ticket {
   const nombre = prompt('Ingrese su nombre');
   const celular = prompt('Ingrese su número de teléfono');
+  const productos = prod;
+
 };
+
+const tickets[];
+tickets.push(ticket)
 */

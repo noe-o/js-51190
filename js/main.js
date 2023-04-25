b85d3c20fd142f6e70a6a15ad120688e2e2c6e05
noe-o <107960650+noe-o@users.noreply.github.com>
2023-04-25 10:05:47 -0300

    3 files modified

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const productos = JSON.parse(localStorage.getItem("productos")) ?? []
+
+const ContenedorProductos = document.querySelector("#aceite")
+productos.forEach(producto => {
+    const tarjetaProducto = document.createElement("div")
+    tarjetaProducto.className = "producto"
+    tarjetaProducto.innerHTML = `
+        <img src="./src/img/tv-with-wide-screen.jpg" alt="">
+        <h3>${producto.nombre}</h3>
+        <p>${producto.descrip}</p>
+        <span>${producto.precio} Pesos</span>
+        <button class="btn">Carrito</button>`
+ContenedorProductos.append(tarjetaProducto)
+})
 /*const carrito = [];
 let total = 0; 
 const compraAceite = () => {

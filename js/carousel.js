3559e32646d6ce469ae24c948eac67cadff30b99
noe-o <107960650+noe-o@users.noreply.github.com>
2023-05-19 14:56:18 -0300

    1 file modified, 1 added

---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+const verCarousel = async () => {
+  try {
+    const res = await fetch('../carrousel.json');
+    const items = await res.json();
+    const carrousel = document.querySelector('#carousel');
+    items.forEach((item) => {
+      const carrouselItem = document.createElement('div');
+      carrouselItem.className = `carousel-item ${item.clases}`;
+      carrouselItem.innerHTML = `
+              <div class="carousel-caption d-none d-md-block">
+                  <h5>${item.nombre}</h5>
+                  <p>${item.detalle}</p>
+              </div>
+          `;
+      carrousel.append(carrouselItem);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+verCarousel();

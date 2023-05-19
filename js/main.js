3f829c2d7575804be6a1a337ace80329fd6863c9
noe-o <107960650+noe-o@users.noreply.github.com>
2023-05-18 22:32:06 -0300

    1 file modified

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-const verProductos = async () =>{
-  const contenedorProductos = document.querySelector("#contenedorProductos")
-  try{
-      const resp = await fetch("./productos.json")
-      const produtos = await resp.json()
-      produtos.forEach(producto => {
-          const tarjetaProducto = document.createElement("div")
-          tarjetaProducto.className = "producto"
-          tarjetaProducto.innerHTML = `<h3>${producto.nombre}</h3>`
-          contenedorProductos.append(tarjetaProducto)
-      });
-  }catch(error){
-
-  }
-}
-verProductos()
+const verProductos = async () => {
+  const contenedorProductos = document.querySelector('#contenedorProductos');
+  try {
+    const resp = await fetch('../productos.json');
+    const produtos = await resp.json();
+    produtos.forEach((producto) => {
+      const tarjetaProducto = document.createElement('div');
+      tarjetaProducto.className = 'producto';
+      tarjetaProducto.innerHTML = `<h3>${producto.nombre}</h3>`;
+      contenedorProductos.append(tarjetaProducto);
+    });
+  } catch (error) {}
+};
+verProductos();

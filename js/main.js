4f9c021c3971fbf3d0aa662e82cffc3f7c02e41d
noe-o <107960650+noe-o@users.noreply.github.com>
2023-05-19 13:54:22 -0300

    2 files modified

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const verProductos = async () => {
   const contenedorProductos = document.querySelector('#contenedorProductos');
   try {
-    const resp = await fetch('../productos.json');
+    const resp = await fetch('./productos.json');
     const produtos = await resp.json();
     produtos.forEach((producto) => {
       const tarjetaProducto = document.createElement('div');
